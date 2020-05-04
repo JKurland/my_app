d63e1c72076fe26d6a887c8fc184adf8be8c10ec
josh <josh.kurland@garrison.com>
2020-05-04 07:42:31 +0100

    No unique_ptr in dynamic

--- dynamic.h
+++ dynamic.h
@@ -2,14 +2,13 @@
 
 #include <type_traits>
 #include <vector>
-#include <memory>
 
 #include "meta.h"
 
 template<typename HandlerT>
 class Dynamic {
 public:
-    Dynamic(std::vector<std::unique_ptr<HandlerT>> handlers): handlers(std::move(handlers)) {}
+    Dynamic(std::vector<HandlerT> handlers): handlers(std::move(handlers)) {}
 
     template<typename CtxT, typename EventT, typename = std::enable_if_t<can_call<HandlerT, CtxT&, EventT>>>
     void operator()(CtxT& ctx, EventT&& event) {
@@ -19,12 +18,12 @@ public:
 
         if (handlers.size() > 1) {
             for (auto handler = begin(handlers); handler != end(handlers) - 1; handler++) {
-                (**handler)(ctx, static_cast<const EventT&>(event));
+                (*handler)(ctx, static_cast<const EventT&>(event));
             }
         }
 
-        (*handlers[handlers.size()-1])(ctx, std::forward<EventT>(event));
+        handlers[handlers.size()-1](ctx, std::forward<EventT>(event));
     }
 private:
-    std::vector<std::unqiue_ptr<HandlerT>> handlers;
+    std::vector<HandlerT> handlers;
 };
